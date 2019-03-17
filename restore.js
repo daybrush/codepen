c8b5a3f046dd32c8e1bd519eda19d539acfcf7aa
Daybrush <daybrush@gmail.com>
2019-03-17 14:26:10 +0900

    fix copy textarea

--- restore.js
+++ restore.js
@@ -54,20 +54,44 @@ ${jsText}
     const button = document.createElement("button");
     const textarea = document.createElement("textarea");
 
-    textarea.addEventListener("focus", e => {
-        textarea.select();
-    });
-    textarea.addEventListener("focus", e => {
+    // textarea.addEventListener("focus", e => {
+    // });
+    textarea.addEventListener("select", e => {
         document.execCommand('copy');
     });
+    function copyTextarea() {
+      const oldContentEditable = textarea.contentEditable;
+      const oldReadOnly = textarea.readOnly;
+      const range = document.createRange();
+
+      textarea.value = restoreText;
+      textarea.contentEditable = true;
+      textarea.readOnly = false;
+      
+      range.selectNodeContents(textarea);
+  
+      var s = window.getSelection();
+      s.removeAllRanges();
+      s.addRange(range);
+  
+      textarea.setSelectionRange(0, textarea.value.length); // A big number, to cover anything that could be inside the element.
+  
+      textarea.contentEditable = oldContentEditable;
+      textarea.readOnly = oldReadOnly;
+
+      textarea.focus();
+      document.execCommand('copy');
+    }
     button.addEventListener("click", e => {
-        if (navigator.clipboard) {
-            navigator.clipboard.writeText(restoreText);
-        } else {
-            textarea.value = restoreText;
-            
-            textarea.focus();            
-        }
+      if (navigator.clipboard && navigator.clipbaord.writeText) {
+        navigator.clipboard.writeText(restoreText).then(e => {
+          alert("copied");
+        }).catch(e => {
+          copyTextarea();
+        });
+      } else {
+        copyTextarea();
+      }
     });
 
     button.style.cssText = "position: absolute; z-index: 10; top: 10px; right: 10px; width: 80px; height: 40px;";
